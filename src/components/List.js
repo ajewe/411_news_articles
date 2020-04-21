a8c8a125e3723a20fe560d6d0adc704695df3de0
ajewe <ajewe@utexas.edu>
2020-04-21 14:29:30 -0500

    finished practice

--- src/components/List.js
+++ src/components/List.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 
 class List extends Component {
     // write componentDidMount function
-    // call "this.props.fetchArticles"
+    componentDidMount() {
+        // call "this.props.fetchArticles"
+        this.props.fetchArticles()
+    }
 
     render() {
         return (
